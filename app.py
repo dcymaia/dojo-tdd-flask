8cd1ffa8abc75c61274984779cd037e946a76519
Danilo Cyrino Maia <dcymaia@gmail.com>
2017-06-26 19:58:18 -0300

    wip test web app

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ meu_web_app = Flask('meu_web_app')
 
 @meu_web_app.route('/')
 def pagina_inicial():
-    return ''
+    return 'Danilo Cyrino Maia'
+
+
+if __name__ == "__main__":
+    meu_web_app.run()
--- tests.py
+++ tests.py
@@ -4,15 +4,18 @@ from app import meu_web_app
 
 class TestHome(unittest.TestCase):
 
-    def test_get(self):
+    def setUp(self):
         app = meu_web_app.test_client()
-        response = app.get('/')
-        self.assertEqual(200, response.status_code)
+        self.response = app.get('/')
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
 
     def test_content_type(self):
-        app = meu_web_app.test_client()
-        response = app.get('/')
-        self.assertIn('text/html', response.content_type)
+        self.assertIn('text/html', self.response.content_type)
+
+    def test_content(self):
+        self.assertIn('Danilo Cyrino Maia', self.response.data.decode('utf-8'))
 
 if __name__ == '__main__':
     unittest.main()
